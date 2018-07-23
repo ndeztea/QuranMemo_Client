5a196b78ba40f4063207d734497d133782fd1bdd
Dimas <dimas.santosa@happyfresh.com>
2018-07-23 09:54:17 +0700

    Header QuranPage

--- src/pages/quran/quran.ts
+++ src/pages/quran/quran.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ModalController,PopoverController,LoadingController} from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController,PopoverController,LoadingController, ViewController} from 'ionic-angular';
 import { RestapiServiceProvider } from '../../providers/restapi-service/restapi-service';
 
 /**
@@ -23,15 +23,25 @@ export class QuranPage {
   currPage : Number;
   nextPage : Number;
   prevPage : Number;
+  surah : String;
+  juz : Number;
 
-
-  constructor(public navCtrl: NavController, public navParams: NavParams, public modalCtrl:ModalController, public popOverCtrl:PopoverController, public restApiService:RestapiServiceProvider, public loadingCtrl: LoadingController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public modalCtrl:ModalController, public popOverCtrl:PopoverController, public restApiService:RestapiServiceProvider, public loadingCtrl: LoadingController, viewCtrl: ViewController) {
     
     let page = this.navParams.get('page');
+    let juz = this.navParams.get('juz');
     page = typeof page == 'undefined'?1:page;
+    juz = typeof juz == 'undefined'?'undefined':juz;
+
     console.log('in');
     this.loadingShow();
-    this.getPages(page);
+    // show juz page if juz is not empty
+    if(juz!='undefined'){
+      this.getJuz(juz);
+    }else{
+      this.getPages(page);
+    }
+    
   }
 
   ionViewDidLoad() {
@@ -49,16 +59,34 @@ export class QuranPage {
   }
 
   getPages(page: number){
-    
     this.restApiService.getAyats(page)
-    .then(data=>{
-      this.ayats = data.ayats;
-      this.currPage = parseInt(data.curr_page);
-      this.nextPage = this.currPage + 1;
-      this.prevPage = this.currPage - 1;
-
+    .then((data:any)=>{
+      if (Object.keys(data).length > 0){
+        this.ayats = data.ayats;
+        this.nextPage = data.next_page;
+        this.prevPage = data.prev_page;
+        this.surah = data.surah;
+        this.juz = data.juz;
+        this.currPage = data.curr_page;
+      }
+      console.log(data);
       this.loading.dismiss();
-      console.log(this.currPage);
+    })
+  }
+
+  getJuz(juz: number){
+    this.restApiService.getAyatJuz(juz)
+    .then((data:any)=>{
+      if (Object.keys(data).length > 0){
+        this.ayats = data.ayats;
+        this.nextPage = data.next_page;
+        this.prevPage = data.prev_page;
+        this.surah = data.surah;
+        this.juz = data.juz;
+        this.currPage = data.curr_page;
+      }
+      console.log(data);
+      this.loading.dismiss();
     })
   }
 
@@ -76,7 +104,7 @@ export class QuranPage {
       spinner: 'bubbles',
       content: `
         <div class="custom-spinner-container">
-          Get data...
+          Getting data...
         </div>`
     });
 
